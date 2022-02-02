e28f755dd559daf6691bcc9157794c45db2325b9
ChanderShekhar <chandershekharofficial@gmail.com>
2022-02-02 13:23:23 +0530

    first commit

--- app/src/main/java/com/example/bankingsystem/MainActivity.java
+++ app/src/main/java/com/example/bankingsystem/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.bankingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.bankingsystem.UI.TransactionHistory;
+import com.example.bankingsystem.UI.UserList;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView bankLogo;
+    TextView tvBankTitle;
+    Button btnAllUsers, btnAllTransactions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        btnAllUsers = findViewById(R.id.all_users);
+        btnAllTransactions = findViewById(R.id.all_transactions);
+        tvBankTitle = findViewById(R.id.bank_title);
+    }
+
+    public void showAllUsers(View view) {
+        Intent intent = new Intent(this, UserList.class);
+        startActivity(intent);
+    }
+
+    public void showAllTransactions(View view) {
+        Intent intent = new Intent(this, TransactionHistory.class);
+        startActivity(intent);
     }
 }
